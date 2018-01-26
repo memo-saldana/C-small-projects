c8840426685020770b13cfb01e516f6403acf2bc
José Guillermo Saldaña Cárdenas <memo.sal98@gmail.com>
2018-01-26 00:09:34 -0600

    Formato de entregas

--- 2nd Semester Activities/1er Parcial/Ejercicios/Ejercio 1 25 Enero/main.cpp
+++ 2nd Semester Activities/1er Parcial/Ejercicios/Ejercio 1 25 Enero/main.cpp
@@ -1,8 +1,19 @@
+//Descripcion: Pide al usuario datos de un producto, su clave, cantidad
+// y precio unitario, y los guarda en un archivo llamad Datos.txt
+//Autor: Jose Guilllermo Saldana Cardenas   A01039888
+//Fecha:25 enero 2018
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 int main(){
+
+/*
+El programa corre un while loop, el cual sigue activo mientras el usuario
+no indique que pare. La variable datos son reutilizadas, pues al guardar la
+informacion que ingresa el usuario, se guardan en el archivo y se puede
+volver a utilizar la variable, ahorrando memoria.
+*/
     char op='s';
     double datos;
     ofstream ArchSal;
@@ -23,6 +34,9 @@ int main(){
         cout<<"Quieres ingresar otro producto? (s/n)"<<endl;
         cin>>op;
     }
+
+    //Se reafirma que se guardo la informacion al  usuario, poniendo un cout
+    // por fuera del while.
 
     cout<<"Informacion guardada correctamente."<<endl;
 
