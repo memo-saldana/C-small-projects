7941fe45d4ef85c06aab1c2e45f9711f1dd8a381
José Guillermo Saldaña Cárdenas <memo.sal98@gmail.com>
2018-01-29 22:30:05 -0600

    Tarea 2 completa

--- 2nd Semester Activities/1er Parcial/Tareas/CopiaLineas/main.cpp
+++ 2nd Semester Activities/1er Parcial/Tareas/CopiaLineas/main.cpp
@@ -8,29 +8,75 @@
 #include <string>
 using namespace std;
 
-void CopiaFichero(string nombre, int iNum){
+/*
+La funcion tiene como proposito copiar las ultimas n lineas dadas por el usuario.
+Los parametros que recibe son el nombre del archivo del cual obtiene la informacion,
+el numero de lineas que quiere copiar, del final hacia el principio, y el nombre del
+archivo al cual se va a copiar.
+La funcion no tiene un valor de retorno, ya que es void.
+*/
+void CopiaFichero(string nombre, int iNum, string nuevo){
     ifstream ArchOr;
+    ofstream ArchNu;
     int lineas=0;
-    ArchOr.open(nombre);
+    string linea;
+
+    ArchOr.open(nombre.c_str());
+    ArchNu.open(nuevo.c_str());
+
+// Primero se cuentan el numero de lineas que tiene el archivo.
     while(!ArchOr.eof()){
+        getline(ArchOr, linea);
         lineas++;
-
-
     }
+// Valida si el archivo tiene suficientes lineas. Notese que se debe cerrar y volver a abrir el archivo ya que el
+// contar las lineas hace que se recorra todo el documento una vez.
+    if(lineas>iNum){
+        ArchOr.close();
+        ArchOr.open(nombre.c_str());
+// Utiliza el numero de lineas para saber cuales lineas copiar y cuales no.
+        for(int i=0;i<lineas;i++){
 
-    cout<<lineas<<endl;
+            getline(ArchOr,linea);
+
+            if(i>=(lineas-iNum)){
+                ArchNu<<linea<<endl;
+            }
+        }
+        cout<<endl<<"Copiado exitoso."<<endl;
+    }
+    else{
+// Mensaje en caso de no poder copiarse por falta de lineas.
+        cout<<"No se puede realizar la copia."<<endl;
+    }
 
 
 }
 int main()
 {
     int n;
-    string nombre;
-    cout<<"Teclea el nombre del archivo."<<endl;
+    string nombre,nuevo;
+    char op;
+
+    cout<<"Teclea el nombre del archivo que copiará."<<endl;
     cin>>nombre;
     cout<<"Teclea las lineas que quieres copiar."<<endl;
     cin>>n;
 
-    CopiaFichero(nombre, n);
+//Da la opcion de darle un nombre al archivo nuevo, en caso de que no
+// sea dado por el usuario, se asigna uno por default.
+    cout<<"Desea ponerle nombre al archivo nuevo? (s/n)"<<endl;
+    cin>>op;
+    if(op!='s'){
+            nuevo="ArchNuevo.txt";
+    }
+    else{
+        cout<<"Teclea el nombre del archivo nuevo."<<endl;
+        cin>>nuevo;
+    }
+
+//Llamado a la funcion
+    CopiaFichero(nombre, n,nuevo);
+
     return 0;
 }
