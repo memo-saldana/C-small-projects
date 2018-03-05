d44669b6cb91e50f18c1f51ade54f3264a5ec673
José Guillermo Saldaña <memo.sal98@gmail.com>
2018-03-05 15:55:17 -0600

    Avance de Paciente.h

--- 2nd Semester Activities/2do Parcial/Proyecto Dos/Pacientes.h
+++ 2nd Semester Activities/2do Parcial/Proyecto Dos/Pacientes.h
@@ -1,9 +1,11 @@
-	Las clases Doctor, Pacientes y Fecha deben tener un constructor default, un constructor con todos los parámetros, métodos de acceso y modificación para todos sus atributos y un método muestra.
-	El atributo listaPx de la clase Cirugías es un arreglo de 10 elementos, en este arreglo se almacena el id de cada uno de los pacientes que tendrán ese tipo cirugia.
-	El atributo cantPx de la clase Cirugías es para almacenar la cantidad de pacientes que requieren esa cirugia, es decir, la cantidad de datos que hay en el arreglo listaPx.
-	La clase Cirugías debe tener:
-o	Constructor default que inicialice la Cirugía con la lista de pacientes vacía y valores default para los demás atributos.
-o	Métodos de acceso y modificación para los atributos de: idDoctor, idCirugía, descripcion, fechaCx, duración.
+/*
+
+Las clases Doctor, Pacientes y Fecha deben tener un constructor default, un constructor con todos los parámetros, métodos de acceso y modificación para todos sus atributos y un método muestra.
+El atributo listaPx de la clase Cirugías es un arreglo de 10 elementos, en este arreglo se almacena el id de cada uno de los pacientes que tendrán ese tipo cirugia.
+El atributo cantPx de la clase Cirugías es para almacenar la cantidad de pacientes que requieren esa cirugia, es decir, la cantidad de datos que hay en el arreglo listaPx.
+La clase Cirugías debe tener:
+Constructor default que inicialice la Cirugía con la lista de pacientes vacía y valores default para los demás atributos.
+de acceso y modificación para los atributos de: idDoctor, idCirugía, descripcion, fechaCx, duración.
 o	Método de acceso para el atributo listaPx, debe recibir como parámetro un índice de cuál paciente (cuál subíndice del arreglo) se quiere accesar.
 o	Debe haber un método para agregar un paciente a la lista de pacientes, éste método debe recibir como parámetro el id del paciente que se va a agregar a una cirugía y el método debe agregarlo al arreglo listaPx y ajustar el atributo cantPx. Es necesario que este método tenga como valor de retorno un valor booleano que indique si se pudo agregar o no el paciente a la cirugía, no se podrá agregar si el paciente se duplica o si el arreglo de pacientes ya está lleno. Una vez que se ejecuta este método, debe enviarse un mensaje indicando si se pudo realizar la alta del paciente o si ya no hay espacio.
 o	El atributo cantPx debe tener solamente método de acceso, porque no es posible modificar la cantidad de pacientes directamente.
@@ -14,9 +16,45 @@ Descripción del Proyecto
 4.	Pide al usuario que teclee los datos para cada uno de los cirugias a realizar, en este momento se dará de alta las cirugias sin pacientes, por lo que la cantidad de pacientes (cantPx) será 0.
 5.	Tu programa deberá tener un menú con las siguientes opciones:
 a.	Consulta de todos las cirugias a realizar, debe mostrar todos los datos generales (sin pacientes) de todos las cirugias que se han registrado.
-b.	Dar de alta un paciente para una cirugia, al hacerlo deberás de validar qu el id del paciente (idPx) y el id de la cirugia (idCirugia) existan y mandar un mensaje de alta exitosa o en caso de que alguno de los dos no existan (paciente y/o cirugia) debe de enviar un mensaje, indicándolo. 
-c.	Consulta de cirugia por fecha, deberás pedir al usuario la fecha (dia, mes, año), validar que sea válida (dia entre 1 y 28 o 30 o 31, mes entre 1 y 12) y mostrar los datos generales (sin pacientes) de todos las cirugias que existen con esa fecha, o un mensaje inidicando que no existen cirugias con dicha fecha.
-d.	Consulta por id de la cirugia, debes validar que el id de la cirugia exista, y en caso afimativo, mostrar en la pantalla los datos generales de la cirugia, así como los nombres de los pacientes que esten dados de alta. En caso de no existir el id de la cirugia, o que no tenga pacientes dados de alta mandar un mensaje indicando lo que sucede.
-e.	Consulta de cirugias de una doctor, debes pedir el idDoctor, validarla y mostrar en la pantalla los datos generales (sin pacientes) de todos las cirugias de dicha doctor. En caso de no existir el doctor o existir pero no tener cirugias, indicarlo con un mensaje según corresponda.
-f.	Consulta la lista de cirugias que se le han practicado a un paciente, debes pedir la id del paciente y mostrar en la pantalla los datos generales de todos las cirugias que se le han realizado a dicho paciente. Muestra los mensajes correspondientes en caso de que el paciente no exista, o en caso de que exista pero no tenga cirugias.
-g.	Terminar.
+Dar de alta un paciente para una cirugia, al hacerlo deberás de validar qu el id del paciente (idPx) y el id de la cirugia (idCirugia) existan y mandar un mensaje de alta exitosa o en caso de que alguno de los dos no existan (paciente y/o cirugia) debe de enviar un mensaje, indicándolo. 
+Consulta de cirugia por fecha, deberás pedir al usuario la fecha (dia, mes, año), validar que sea válida (dia entre 1 y 28 o 30 o 31, mes entre 1 y 12) y mostrar los datos generales (sin pacientes) de todos las cirugias que existen con esa fecha, o un mensaje inidicando que no existen cirugias con dicha fecha.
+dConsulta por id de la cirugia, debes validar que el id de la cirugia exista, y en caso afimativo, mostrar en la pantalla los datos generales de la cirugia, así como los nombres de los pacientes que esten dados de alta. En caso de no existir el id de la cirugia, o que no tenga pacientes dados de alta mandar un mensaje indicando lo que sucede.
+eConsulta de cirugias de una doctor, debes pedir el idDoctor, validarla y mostrar en la pantalla los datos generales (sin pacientes) de todos las cirugias de dicha doctor. En caso de no existir el doctor o existir pero no tener cirugias, indicarlo con un mensaje según corresponda.
+fConsulta la lista de cirugias que se le han practicado a un paciente, debes pedir la id del paciente y mostrar en la pantalla los datos generales de todos las cirugias que se le han realizado a dicho paciente. Muestra los mensajes correspondientes en caso de que el paciente no exista, o en caso de que exista pero no tenga cirugias.
+gTerminar.
+
+
+*/
+
+#ifndef Pacientes_h
+#define Pacientes_h
+
+class Paciente{
+
+
+private:
+	int idPx,edad;
+	string nombre,genero;
+
+public:
+	Pacientes();
+	Pacientes(int idPx, string nombre, string genero, int edad);
+
+	void getIdPx();
+	void getNombre();
+	void getGenero();
+	void getEdad();
+	
+	
+	void muestra();
+
+
+
+	
+};
+
+
+
+#endif /* Pacientes_h */
+
+
